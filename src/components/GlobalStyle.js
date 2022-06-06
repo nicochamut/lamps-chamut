587644fa3cdac1d2a58b5ef5c68702ed9e46d949
nicochamut <nicochamut@gmail.com>
2022-06-06 15:35:31 -0300

    2

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html {
+    @media (max-width: 1700px){
+        font-size: 75%
+    }
+}
+
+body {
+    background:#ffffff;
+    font-family: 'Inter', sans-serif;
+    overflow-x: hidden;
+    color: white;
+   
+    
+}
+
+`
+
+export default GlobalStyle
